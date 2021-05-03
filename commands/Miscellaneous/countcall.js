78d8416cb307e6734203fb76f36bef7aeaa770a6
RootGC <matheusbzo@hotmail.com>
2021-05-03 20:47:19 -0300

    📝 corrigindo bug countcall

--- commands/Miscellaneous/countcall.js
+++ commands/Miscellaneous/countcall.js
@@ -38,7 +38,7 @@ module.exports = class CountCall extends Command {
         for (const embed of embeds) {
             message.channel.send(embed);
 
-        if (users.length < 1){
+        if(users.length < 0){
         message.channel.send('Talvez aqui não esteja ninguém em call.').then(msg => msg.delete(5000))
     }
 };
